2b9dccf33cf211e0ddbc9ffae3cc885451feabbe
Даня <Danya@MacBook-Air-User.local>
2022-11-06 23:36:05 +0400

    первый коммит для спринта 7

---
+++ exceptions.py
@@ -0,0 +1,34 @@
+class ServiceError(Exception):
+    """Ошибка отсутствия доступа по заданному эндпойнту."""
+
+    pass
+
+
+class EndpointError(Exception):
+    """Ошибка, если эндпойнт не корректен."""
+
+    pass
+
+
+class MessageSendingError(Exception):
+    """Ошибка отправки сообщения."""
+
+    pass
+
+
+class GlobalsError(Exception):
+    """Ошибка, если есть пустые глобальные переменные."""
+
+    pass
+
+
+class DataTypeError(Exception):
+    """Ошибка, если тип данных не dict."""
+
+    pass
+
+
+class ResponseFormatError(Exception):
+    """Ошибка, если формат response не json."""
+
+    pass
--- homework.py
+++ homework.py
@@ -1,16 +1,40 @@
-...
+import logging
+import os
+import time
+from pathlib import Path
+
+from exceptions import ServiceError, EndpointError,\
+MessageSendingError, GlobalsError, DataTypeError, ResponseFormatError
+
+import requests
+import telegram
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
+CONNECTION_ERROR = '{error}, {url}, {headers}, {params}'
+SERVICE_REJECTION = '{code}'
+WRONG_ENDPOINT = '{response_status}, {url}, {headers}, {params}'
+WRONG_HOMEWORK_STATUS = '{homework_status}'
+WRONG_DATA_TYPE = 'Неверный тип данных {type}, вместо "dict"'
+STATUS_IS_CHANGED = '{verdict}, {homework}'
+STATUS_IS_NOT_CHANGED = 'Статус не изменился, нет записей'
+FAILURE_TO_SEND_MESSAGE = '{error}, {message}'
+GLOBAL_VARIABLE_IS_MISSING = 'Отсутствует глобальная переменная'
+GLOBAL_VARIABLE_IS_EMPTY = 'Пустая глобальная переменная'
+MESSAGE_IS_SENT = 'Сообщение {message} отправлено'
+FORMAT_NOT_JSON = 'Формат не json {error}'
+LIST_IS_EMPTY = 'Список пуст'
 
 HOMEWORK_STATUSES = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
@@ -20,64 +44,109 @@ HOMEWORK_STATUSES = {
 
 
 def send_message(bot, message):
-    ...
-
+    """Отправляет сообщение пользователю в Телегу."""
+    try:
+        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+    except Exception as error:
+        raise MessageSendingError(FAILURE_TO_SEND_MESSAGE.format(
+            error=error,
+            message=message,
+        ))
+    logging.info(f'Message "{message}" is sent')
 
 def get_api_answer(current_timestamp):
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-
-    ...
-
+    all_params = dict(url=ENDPOINT, headers=HEADERS, params=params)
+    try:
+        response = requests.get(**all_params)
+    except requests.exceptions.RequestException as error:
+        raise telegram.TelegramError(CONNECTION_ERROR.format(
+            error=error,
+            **all_params,
+        ))
+    response_status = response.status_code
+    if response_status != 200:
+        raise EndpointError(WRONG_ENDPOINT.format(
+            response_status=response_status,
+            **all_params,
+        ))
+    try:
+        return response.json()
+    except Exception as error:
+        raise ResponseFormatError(FORMAT_NOT_JSON.format(error))
 
 def check_response(response):
-
-    ...
-
+    """
+    Возвращает домашку, если есть.
+    Проверяет валидность её статуса.
+    """
+    if 'code' in response:
+        raise ServiceError(SERVICE_REJECTION.format(
+            code=response.get('code'),
+        ))
+    if response['homeworks']:
+        return response['homeworks'][0]
+    else:
+        raise IndexError(LIST_IS_EMPTY)
 
 def parse_status(homework):
-    homework_name = ...
-    homework_status = ...
+    """Возвращает текст сообщения от ревьюера."""
+    if not isinstance(homework, dict):
+        raise DataTypeError(WRONG_DATA_TYPE.format(type(homework)))
+    homework_name = homework.get('homework_name')
+    homework_status = homework.get('status')
 
-    ...
+    if homework_status not in HOMEWORK_STATUSES:
+        raise NameError(WRONG_HOMEWORK_STATUS.format(homework_status))
 
-    verdict = ...
-
-    ...
+    verdict = HOMEWORK_STATUSES[homework_status]
 
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
-
 def check_tokens():
-    ...
-
+    """Проверяет доступность переменных окружения."""
+    for key in (PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID, ENDPOINT):
+        if key is None:
+            logging.error(GLOBAL_VARIABLE_IS_MISSING)
+            return False
+        if not key:
+            logging.error(GLOBAL_VARIABLE_IS_EMPTY)
+            return False
+    return True
 
 def main():
     """Основная логика работы бота."""
-
-    ...
-
+    if not check_tokens():
+        raise GlobalsError('Ошибка глобальной переменной. Смотрите логи.')
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
 
-    ...
-
     while True:
         try:
-            response = ...
-
-            ...
-
-            current_timestamp = ...
-            time.sleep(RETRY_TIME)
-
+            response = get_api_answer(current_timestamp)
+            homework = check_response(response)
+            message = parse_status(homework)
+            send_message(bot, message)
+            logging.info(homework)
+            current_timestamp = response.get('current_date')
+        except IndexError:
+            message = 'Статус работы не изменился'
+            send_message(bot, message)
+            logging.info(message)
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
+            send_message(bot, message)
+        finally:
             time.sleep(RETRY_TIME)
-        else:
-            ...
+            logging.info(MESSAGE_IS_SENT.format(message))
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        format='%(asctime)s, %(message)s, %(lineno)d, %(name)s',
+        filemode='w',
+        filename=f'{Path(__file__).stem}.log',
+        level=logging.INFO,
+    )
     main()
