44bfd0119e06d19dfa96e48abbf551da5dac5674
Даня <Danya@MacBook-Air-User.local>
2022-11-07 02:08:19 +0400

    pep8

--- homework.py
+++ homework.py
@@ -56,7 +56,7 @@ def send_message(bot, message):
 
 
 def get_api_answer(current_timestamp):
-    """Получает ответ от API"""
+    """Получает ответ от API."""
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
     all_params = dict(url=ENDPOINT, headers=HEADERS, params=params)
